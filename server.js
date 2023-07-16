ea1a9b1e512cd045b1ff6b2e497e97ec988eef7d
Mohdashraf7078 <mohdashraf1217@gmail.com>
2023-07-16 00:11:17 -0700

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+const PORT = 4000;
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.use(expressLayout);
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/resources/views'))
+app.listen(PORT, ()=> {
+    console.log(`running on port ${PORT}`)
+})
