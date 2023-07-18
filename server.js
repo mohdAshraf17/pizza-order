fe3545fe8aa597683ab26388f44fce244d480b88
Mohdashraf7078 <mohdashraf1217@gmail.com>
2023-07-18 04:53:20 -0700

    product, auth

--- server.js
+++ server.js
@@ -6,14 +6,23 @@ const path = require('path')
 const PORT = 4000;
 
 
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
 app.use(expressLayout);
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/resources/views'))
-app.listen(PORT, ()=> {
+
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/cart', (req, res) => {
+    res.render('cart')
+})
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+app.listen(PORT, () => {
     console.log(`running on port ${PORT}`)
 })
