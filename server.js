cc2b13af9576bfb3941bde762aa6f37f32131b58
Mohdashraf7078 <mohdashraf1217@gmail.com>
2024-02-24 04:56:35 -0800

    fix

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connection.once('open', () => {
     console.log('database connected')
 });
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 app.use(expressLayout);
 app.set('view engine', 'ejs')
